06d71b477b726d425eeb9e564c32c51189317d0e
NargizHasanova <nargiz.gasanova.1996@mail.ru>
2021-12-15 02:19:15 +0400

    commit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,29 +1,51 @@
-const submitForm = document.querySelector(".submit-form-btn")
-const title = document.querySelector("#validationDefault01").value
-const text = document.querySelector("#form-textarea").innerHTML
-const img = "img"
-const date = "date"
+const submitBtn = document.querySelector(".submit-form-btn")
+const title = document.querySelector("#validationDefault01")
+const text = document.querySelector("#form-textarea")
+const img = document.querySelector("#validationDefault03")
+const clear = document.querySelector(".clearAPI")
 const id = Date.now()
+const date = new Date()
 
-function handleSubmit() {
-    //e.preventDefault()
-    fetch('https://test-003-69327-default-rtdb.firebaseio.com/news.json', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
+let newImgResult = null
+img.addEventListener("change", (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onloadend = () => {
+        newImgResult = reader.result
+    }
+    reader.readAsDataURL(file)
+})
 
-        body: JSON.stringify({
-            title: title,
-            img: img,
-            text: text,
-            date: date
+submitBtn.addEventListener("click", () => {
+    axios.post("https://test-003-69327-default-rtdb.firebaseio.com/news.json",
+        {
+            id: id, title: title.value, text: text.value, img: newImgResult, date: date
         })
-    })
-        .then(res => res.json())
-        .then(res => console.log(res));
-}
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+    //location.reload()
+})
 
 
-//handleSubmit()
+
+// submitBtn.addEventListener("click", () => {
+//     fetch('https://test-003-69327-default-rtdb.firebaseio.com/news.json', {
+//         method: 'post',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+
+//         body: JSON.stringify({
+//             title: title.value,
+//             img: newImgResult,
+//             text: text.value,
+//             date: date
+//         })
+//     })
+//         .then(res => res.json())
+//         .then(res => console.log(res));
+// })
+
+
+
--- assets/js/news.js
+++ assets/js/news.js
@@ -1,35 +1,59 @@
 const newsCover = document.querySelector(".news-cover")
+let newArr = []
 
-
-
-fetch('https://test-003-69327-default-rtdb.firebaseio.com/news.json')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        latestNews(data)
-    });
-
-function latestNews(data) {
-    let allData = data.map(item => {
-        const { id, img, title, text, date } = item
-        return (
-            `
-                <div key=${id} class="swiper-slide news-item">
-                    <img src=${img} />
-                    <div class="news-desc">
-                        <span>${date}</span>
-                        <h2>${title}</h2>
-                        <p>${text}</p>
-                        <div class="shop-btn"><button>Read more</button></div>
+axios.get("https://test-003-69327-default-rtdb.firebaseio.com/news.json")
+    .then(res => res.data).then(data => {
+        for (let key in data) {
+            newArr.push(data[key])
+        }
+        let a = newArr.map(item => {
+            return (
+                `
+                    <div key=${item.id} class="swiper-slide news-item">
+                            <img src=${item.img} alt=${item.title} />
+                            <div class="news-desc">
+                                <span>${item.date}</span>
+                                <h2>${item.title}</h2>
+                                <p>${item.text}</p>
+                                <div class="shop-btn"><button>Read more</button></div>
+                            </div>
                     </div>
-                </div>
-            `
-        )
+                `)
+        })
+        newsCover.innerHTML = a.join("")
+
     })
-    newsCover.innerHTML = allData.join(" ")
-}
+    .catch(error => console.log(error))
+
+
+// fetch('https://test-003-69327-default-rtdb.firebaseio.com/news.json')
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         latestNews(data)
+//     });
+
+// function latestNews(data) {
+//     let allData = data.map(item => {
+//         const { id, img, title, text, date } = item
+//         return (
+//             `
+//                 <div key=${id} class="swiper-slide news-item">
+//                     <img src=${img} />
+//                     <div class="news-desc">
+//                         <span>${date}</span>
+//                         <h2>${title}</h2>
+//                         <p>${text}</p>
+//                         <div class="shop-btn"><button>Read more</button></div>
+//                     </div>
+//                 </div>
+//             `
+//         )
+//     })
+//     newsCover.innerHTML = allData.join(" ")
+// }
 
 
 
